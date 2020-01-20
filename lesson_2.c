52f8b20c04d3d1077a0a6c615c121ef99a9104e2
BespalchukDY <founder.10@mail.ru>
2020-01-20 16:14:37 +0500

    Выполнил задания ко второму уроку

--- lesson_2.c
+++ lesson_2.c
@@ -5,12 +5,17 @@
 //Задание выполнил Беспальчук Денис Юрьевич
 
 void task1();
-void task2();
-void task3();
-void menu();
-void Func(int n);
-double power(double x, long n);
+void DecBin(int n);
 
+void task2();
+double power(double x, long n);
+double power_rec_even(double x, long n);
+
+void task3();
+int calc_arr(void);
+int calc_rec(int);
+
+void menu();
 
 int main()
 {
@@ -49,23 +54,21 @@ int main()
 */
 void task1()
 {
-		int n;
+	int n;
 
-		printf("Введите десятичное число: ");
-		scanf("%d", &n);
+	printf("Введите десятичное число: ");
+	scanf("%d", &n);
 
-		Func(n);
-		printf("\n");
-		return 0;
+	DecBin(n);
+	printf("\n");
 }
 
-void Func(int n) {
+void DecBin(int n) {
 
 	if (n != 0)
-		Func(n / 2);
-	else return;
+		DecBin(n / 2);
+
 	printf("%d", n % 2);
-	return;
 }
 
 /*
@@ -76,10 +79,8 @@ b. Рекурсивно.
 c. *Рекурсивно, используя свойство чётности степени.
 */
 
-
-//Задание 2 (без рекурсии)
-
-
+//Задание 2a (без рекурсии)
+/*
 double power(double x, long n)
 {
 	double a = 1;
@@ -95,32 +96,52 @@ double power(double x, long n)
 	}
 	return a;
 }
+*/
 
 
+//Задание 2b (с рекурсией)
 
-//Задание 2 (с рекурсией)
-/*
 double power(double x, long n)
 {
-	double tmp;
 	if (n == 0) return 1;
 	if (n < 0) return power(1 / x, -n);
 	if (n % 2) return x * power(x, n - 1);
 	return power(x * x, n / 2);
 }
-*/
+
+//Задание 3c (с рекурсией + свойство четности степени)
+double power_rec_even(double x, long n) {
+	long p = 1;
+
+	if (n == 0) {
+		return 1;
+	}
+
+	if (n % 2 == 0) {
+		p = power_rec_even(x, n / 2);
+		return p * p;
+	}
+	else {
+		return x * power_rec_even(x, n - 1);
+	}
+
+}
+
 
 void task2()
 {
 	double x;
 	long n;
+
 	printf("Введите число, которое хотите возвести в степень: \n");
 	scanf("%lf", &x);
 
 	printf("Введите степень: \n");
 	scanf("%ld", &n);
 
-	printf("%16.3lf\n", power(x, n));
+	printf("%.2lf\n", power(x, n));
+
+	printf("Посчитали с помощью рекурсии и свойством четности степени: %.2lf\n\n", power_rec_even(x, n));
 }
 
 /*
@@ -134,9 +155,43 @@ void task2()
 а. С использованием массива.
 b. *С использованием рекурсии.
 */
+
+// реализация через массив
+int calc_arr(void) {
+	int a[21];
+	int i;
+
+	a[2] = 0;
+	a[3] = 1;
+
+	for (i = 4; i < 21; i++) {
+		if (i % 2 == 0) {
+			a[i] = a[i - 1] + a[i / 2];
+		}
+		else {
+			a[i] = a[i - 1];
+		}
+	}
+
+	return a[20];
+}
+
+// реализация с рекурсией
+int calc_rec(int i) {
+	if (i == 2) return 0;
+	if (i == 3) return 1;
+	if (i % 2 == 0) {
+		return calc_rec(i - 1) + calc_rec(i / 2);
+	}
+	else {
+		return calc_rec(i - 1);
+	}
+}
+
 void task3()
 {
-
+	printf("Через массив: %d\n", calc_arr());
+	printf("С помощью рекурсии: %d\n\n", calc_rec(20));
 }
 
 void menu()
